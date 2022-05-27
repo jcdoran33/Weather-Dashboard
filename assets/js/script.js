48e0baf6c4f4585b522b1381ae575e8cb859ba5a
Jack Doran <jcdoran33@gmail.com>
2022-05-26 21:32:01 -0400

    html skeleton

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+console.log("Javascript is linked!")
+
+
+// need to reference two apis, the one call weather api, AND
+// another apis that will go fetch a city's lat and lon based on a text city name
+//maybe google maps api?
+
+
+// create an IF conditional statement, that will apply an accurate weather icon based on weather criteria, in the 5 day forecast
